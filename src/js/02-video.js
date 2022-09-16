ebf330d064ffa4b49df1c5508b5f49db00f91c99
AlexanderTobert <tobert.sasha@gmail.com>
2022-09-16 23:24:55 +0300

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import throttle from 'lodash.throttle';
+import player from "@vimeo/player";
+
+const vimeoPlayerRef = document.querySelector('#vimeo-player');
+const playerRef = new player (vimeoPlayerRef);
+// console.log(vimeoPlayerRef);
+// console.log(playerRef);
+
+
+const onPlay = function({ seconds }) {
+    // data is an object containing properties specific to that event
+    
+    localStorage.setItem('videoplayer-current-time', seconds)
+    // console.log(data);
+    console.log(seconds);
+};
+
+playerRef.on('timeupdate', throttle(onPlay, 1000));
+
+playerRef.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
+const formRef = document.querySelector('.feedback-form');
+formRef.addEventListener('input', throttle(formChange, 500))
 
+console.log(formRef);
+
+const dataForm = {};
+
+function formChange(e) {
+    //отслеживаем событие в форме и вносим изменения значей по ключу в объекте dataForm
+    dataForm[e.target.name] = e.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(dataForm));
+}
+
+formRef.addEventListener('submit', submitForm);
+
+function submitForm(e) {
+    //отслеживаем события отправки формы и вывод объекта в консоль + очистка формы и localStorage
+    console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+    e.preventDefault();
+    e.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state');
+}
+
+// функция сохранения данных формы в случае обновления страницы
+function formStorage() {
+    const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+    const emailRef = formRef.querySelector('input');
+    const messageRef = formRef.querySelector('textarea');
+    
+    // console.log(data);
+
+    if (data) {
+        if (data.email !== undefined) {
+            emailRef.value = data.email;
+        } else {
+            emailRef.value = '';
+        }
+        if (data.message !== undefined) {
+            messageRef.value = data.message;
+        } else {
+            messageRef.value = '';
+        }
+    }
+};
+
+formStorage();
