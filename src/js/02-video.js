a35bda666257fbdc1bbcbf85f118822d231e6dc6
AlexanderTobert <tobert.sasha@gmail.com>
2022-09-18 23:16:16 +0300

    fixed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,6 +6,18 @@ const playerRef = new player (vimeoPlayerRef);
 // console.log(vimeoPlayerRef);
 // console.log(playerRef);
 
+let parseTime;
+
+function checkTime() {
+    const saveTime = localStorage.getItem('videoplayer-current-time')
+    if (saveTime) {
+        parseTime = saveTime;
+    } else {
+        parseTime = 0;
+    }
+}
+
+checkTime();
 
 const onPlay = function({ seconds }) {
     // data is an object containing properties specific to that event
@@ -17,5 +29,5 @@ const onPlay = function({ seconds }) {
 
 playerRef.on('timeupdate', throttle(onPlay, 1000));
 
-playerRef.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+playerRef.setCurrentTime(parseTime)
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,14 +1,20 @@
 import throttle from 'lodash.throttle';
 const formRef = document.querySelector('.feedback-form');
+const emailRef = formRef.querySelector('input');
+const messageRef = formRef.querySelector('textarea');
+
 formRef.addEventListener('input', throttle(formChange, 500))
 
-console.log(formRef);
+// console.log(formRef);
 
 const dataForm = {};
 const STORAGE_KEY = 'feedback-form-state';
 
+formStorage();
+
 function formChange(e) {
     //отслеживаем событие в форме и вносим изменения значей по ключу в объекте dataForm
+
     dataForm[e.target.name] = e.target.value;
     localStorage.setItem(STORAGE_KEY, JSON.stringify(dataForm));
 }
@@ -17,32 +23,27 @@ formRef.addEventListener('submit', submitForm);
 
 function submitForm(e) {
     //отслеживаем события отправки формы и вывод объекта в консоль + очистка формы и localStorage
+    if (emailRef.value === "" || messageRef.value === "") {
+        alert("Необходимо заполнить все поля!");
+        return;
+    }
     console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
     e.preventDefault();
     e.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
+
 }
 
 // функция сохранения данных формы в случае обновления страницы
 function formStorage() {
-    const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
-    const emailRef = formRef.querySelector('input');
-    const messageRef = formRef.querySelector('textarea');
-    
+    let data = localStorage.getItem(STORAGE_KEY);
     // console.log(data);
-
     if (data) {
-        if (data.email !== undefined) {
-            emailRef.value = data.email;
-        } else {
-            emailRef.value = '';
-        }
-        if (data.message !== undefined) {
-            messageRef.value = data.message;
-        } else {
-            messageRef.value = '';
-        }
+        data = JSON.parse(data);
+        Object.entries(data).forEach(([name, value]) => {
+            dataForm[name] = value;
+            formRef.elements[name].value = value;
+        })
     }
 };
 
-formStorage();
